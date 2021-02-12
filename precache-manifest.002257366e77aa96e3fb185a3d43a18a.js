d93411a42c713f43ec6d59c2cf2b02b1cdd39b6b
Pallavi Mahalingaiah <pallavimahalingaiah@Pallavis-MacBook-Pro.local>
2021-02-12 21:02:59 +0530

    Updated resume and case studies

---
+++ precache-manifest.002257366e77aa96e3fb185a3d43a18a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b87a86e30408fbc084a391413669b123",
+    "url": "/index.html"
+  },
+  {
+    "revision": "793f10ee82b51bef5c18",
+    "url": "/static/css/main.194b9b28.chunk.css"
+  },
+  {
+    "revision": "89dcfa6793becc34533c",
+    "url": "/static/js/2.4dbd65e2.chunk.js"
+  },
+  {
+    "revision": "793f10ee82b51bef5c18",
+    "url": "/static/js/main.ce9c5941.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "3b11dde5a8a743879c2b1e0d063fd64d",
+    "url": "/static/media/notion.3b11dde5.svg"
+  },
+  {
+    "revision": "f0af68421a938ee9eca86536ef1087f5",
+    "url": "/static/media/resume.f0af6842.svg"
+  }
+]);
